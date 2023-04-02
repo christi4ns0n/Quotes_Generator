554645744d9996f22541dcb7f2f2b21fb0c99230
christi4ns0n <404webo@gmail.com>
2023-04-02 23:32:16 +0300

    initial commit

--- script.js
+++ script.js
@@ -1,1 +1,93 @@
-console.log('test');
+const quoteContainer = document.getElementById('quote-container');
+const quoteText = document.getElementById('quote');
+const authorText = document.getElementById('author');
+const twitterBtn = document.getElementById('twitter');
+const facebookBtn = document.getElementById('facebook');
+const newQuoteBtn = document.getElementById('new-quote');
+const loader = document.getElementById('loader');
+
+// Show loading
+function loading() {
+    loader.hidden = false;
+    quoteContainer.hidden = true;
+}
+// Hide loading
+function complete() {
+    quoteContainer.hidden = false;
+    loader.hidden = true;
+}
+
+// API Fetch!!!
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+    loading();
+// Pick a random quote from apiQuotes array
+    const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+    // Check if author field is blank and replace with unknown
+    if (!quote.author) {
+        authorText.textContent = 'Unknown'
+    } else {
+        authorText.textContent = quote.author;
+    }
+    // Check Quote Length to determine styling
+    if (quote.text.length > 120) {
+        quoteText.classList.add('long-quote');
+    } else {
+        quoteText.classList.remove('long-quote');
+    }
+    //Set Quote, Hide Loader 
+    quoteText.textContent = quote.text;
+    complete();
+}
+
+// Get Quotes From API
+async function getQuotes() {
+    loading();
+    const apiUrl = 'https://type.fit/api/quotes';
+    try {
+     const response = await fetch(apiUrl);
+     apiQuotes = await response.json();
+     newQuote();  
+    }catch (error){
+        // Catch Error Here
+    }
+}
+
+// Tweet Quote
+function tweetQuote() {
+    const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(twitterUrl, '_blank');
+}
+
+// Facebook Share
+function shareQuote() {
+    const facebookUrl = `https://www.facebook.com/dialog/share?app_id=145634995501895&display=popup&href=https%3A%2F%2Fdevelopers.facebook.com%2Fdocs%2F&redirect_uri=https%3A%2F%2Fdevelopers.facebook.com%2Ftools%2Fexplorer?text=${quoteText.textContent} - ${authorText.textContent}`;
+    window.open(facebookUrl, '_blank');
+}
+
+// Event Listeners
+newQuoteBtn.addEventListener('click', newQuote);
+twitterBtn.addEventListener('click', tweetQuote);
+facebookBtn.addEventListener('click', shareQuote);
+
+
+// On Load
+getQuotes();
+
+
+
+
+// Local !!!
+
+// // Show New Quote
+// function newQuote() {
+// // Pick a random quote from apiQuotes array
+// const quote = localQuotes[Math.floor(Math.random() * localQuotes.length)];
+// console.log(quote);
+// }
+
+// // On Load
+// newQuote();
